33efeddd202326b4613e5bde28cdb4e5696285ff
Ian Boden <ianboden@uk.ibm.com>
2022-06-14 12:01:06 +0100

    copyright and renames

--- operator/roles/amlen/library/configure.py
+++ operator/roles/amlen/library/configure.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python
+# Copyright (c) 2022 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
 from ansible.module_utils.basic import *
 import socket
 import subprocess
