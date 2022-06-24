be8eadbafa18299c19ba67d69f5193d9e8866f43
Jon Levell <levell@uk.ibm.com>
2022-06-24 12:43:11 +0100

    [patch] Make the example API keys more clearly examples to avoid false positives

--- svt_tools/scripts/python/svt_iotf_bulk_register.bat
+++ svt_tools/scripts/python/svt_iotf_bulk_register.bat
@@ -4,17 +4,17 @@ REM BAT file to run svt-register-iotf to register all of the necessary organizat
 REM first we set up the environment variables we need
 REM Expected environment variables & example values: 
 
-SET ORG_OWNER=imaperf@us.ibm.com
+SET ORG_OWNER=someone@example.com
 
-SET ORG_NAME=w7vwxv
-set API_KEY="a-w7vwxv-dixplhfggn"
-SET API_TOKEN="bUjVV5D9hQG9dzoH-d"
+SET ORG_NAME=org123
+set API_KEY=a-org123-example123
+SET API_TOKEN=exampleExample123
 
 SET FIRST_DEVICE_ID=0
 SET LAST_DEVICE_ID=0
 
-REM SET REST_URL=staging.internetofthings.ibmcloud.com/api/v0001
-SET REST_URL="lon02-1.test.internetofthings.ibmcloud.com/api/v0001"
+REM SET REST_URL=test.iot.example.com/api/v0001
+SET REST_URL=test.iot.example.com/api/v0001
 SET NUM_THREADS=1
 REM SET CSV_DEVICE_FILE=next20kdevices.csv
 SET CSV_DEVICE_FILE=missingCars.csv
