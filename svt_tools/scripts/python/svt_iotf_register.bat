a868b5c2cdb75c60d710b287d08b38950f3ed010
Jon Levell <jonquark@users.noreply.github.com>
2022-06-24 14:45:53 +0100

    [patch] Make the example API keys more clearly examples to avoid false positives (#64) (#65)

--- svt_tools/scripts/python/svt_iotf_register.bat
+++ svt_tools/scripts/python/svt_iotf_register.bat
@@ -4,24 +4,24 @@ REM BAT file to run svt-register-iotf to register all of the necessary organizat
 REM first we set up the environment variables we need
 REM Expected environment variables & example values: 
 
-SET ORG_OWNER=imaperf@us.ibm.com
+SET ORG_OWNER=someone@example.com
 
-REM SET ORG_NAME=dxdwqs
-REM SET API_KEY=a-dxdwqs-6xzmkugb93
-REM SET API_TOKEN=wSON-Grf16*kNSasC5
+REM SET ORG_NAME=org123
+REM SET API_KEY=a-org123-example123
+REM SET API_TOKEN=exampleExample123
 
-REM SET ORG_NAME=nwxtmk
-REM SET API_KEY=a-nwxtmk-s1bmnuh5ho
-REM SET API_TOKEN=Ot89XoKfqrrnlN3bR4
+REM SET ORG_NAME=org123
+REM SET API_KEY=a-org123-example123
+REM SET API_TOKEN=exampleExample123
 
 REM TEST ORGANIZATION AND KEYS
-SET ORG_NAME=nnvgkj
-SET API_KEY="a-nnvgkj-cnqtzpikn8"
-SET API_TOKEN="vDgHTD_jUj3ahpOpCH"
+SET ORG_NAME=org123
+SET API_KEY=a-org123-example123
+SET API_TOKEN=exampleExample123
 
 
-REM SET REST_URL=staging.internetofthings.ibmcloud.com/api/v0001
-SET REST_URL="lon02-1.test.internetofthings.ibmcloud.com/api/v0001"
+REM SET REST_URL=test.iot.example.com/api/v0001
+SET REST_URL=test.iot.example.com/api/v0001
 
 SET NUM_THREADS=255
 
