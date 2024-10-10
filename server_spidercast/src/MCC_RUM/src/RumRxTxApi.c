346cfa817c3ef2d3f97d497a67c0dcce664de563
Chitrangi Jog <cjog03@gmail.com>
2024-10-10 16:07:59 +0530

    Update RumRxTxApi.c

--- server_spidercast/src/MCC_RUM/src/RumRxTxApi.c
+++ server_spidercast/src/MCC_RUM/src/RumRxTxApi.c
@@ -126,10 +126,13 @@ static void rumImutex_unlock(void)
 
 static int rumInstanceCounter = 1;
 static char* createRUMInstanceName(void){
-  char  result[64];
-  snprintf(result,64,"RUM.%d",rumInstanceCounter);
+  char* result = malloc(64 * sizeof(char)); //to dynamically allocate 64 bytes 
+  if (result == NULL){
+    return NULL;
+  }
+  snprintf(result, 64, "RUM.%d", rumInstanceCounter);
   rumInstanceCounter++;
-  return rmm_strdup(result);
+  return result;
 }
 
 static void registerRUMTraceComponents(const char* instanceName, TCHandle tcHandles[3]){
